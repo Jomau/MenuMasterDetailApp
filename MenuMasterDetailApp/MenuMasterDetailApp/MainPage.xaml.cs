0ea14f573e97c3171a19af7a6c7dab0620f49f44
Jomau <jomau05@gmail.com>
2018-05-03 02:25:26 -0500

    Ingreso de código

--- MenuMasterDetailApp/MenuMasterDetailApp/MainPage.xaml.cs
+++ MenuMasterDetailApp/MenuMasterDetailApp/MainPage.xaml.cs
@@ -7,11 +7,22 @@ using Xamarin.Forms;
 
 namespace MenuMasterDetailApp
 {
-	public partial class MainPage : ContentPage
+	public partial class MainPage : MasterDetailPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
+            masterPage.ListView.ItemSelected += ListView_ItemSelected;
 		}
-	}
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as MasterPageItem;
+            if (item != null) {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TipoPagina));
+                masterPage.ListView.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+    }
 }
